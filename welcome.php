e1247c03c1b3c89e0ae4c3a1150e71efea6ea8c6
bethnixon94 <138825934+bethnixon94@users.noreply.github.com>
2023-07-21 22:25:55 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="contact.html" />
+	<title>Success!</title>
+</head>
+<body>
+
+<?php
+Thank you, <?php echo $_POST["fullname"]; ?><br>
+We will contact you at your email address: <?php echo $_POST["email"]; ?><br>
+Please allow 1-2 business days for a response. We look forward to working with you! ?>
+
+</body>
+</html>
